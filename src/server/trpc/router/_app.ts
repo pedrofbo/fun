4002609adaf6554dfecf372b98808ecec0831c54
pedrofbo <pedrofelipebo@gmail.com>
2023-01-24 23:53:42 -0300

    chore: remove unused router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,12 +1,10 @@
 import { router } from "../trpc";
-import { boomRouter } from "./boom";
 import { exampleRouter } from "./example";
 import { funRouter } from "./funFact";
 import { pokemonRouter } from "./pokemon";
 
 export const appRouter = router({
   example: exampleRouter,
-  boom: boomRouter,
   pokemon: pokemonRouter,
   fun: funRouter
 });

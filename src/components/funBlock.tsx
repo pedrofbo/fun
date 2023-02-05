ebf444a3c9b06183a7314567c5bab6c72a86f62d
pedrofbo <pedrofelipebo@gmail.com>
2023-02-04 21:24:52 -0300

    feat: test page metadata

--- src/components/funBlock.tsx
+++ src/components/funBlock.tsx
@@ -6,13 +6,15 @@ import Header from "./header";
 import { trpc } from "../utils/trpc";
 
 export const FunBlock = (date: string) => {
-  const funFactOfTheDay = trpc.fun.getFunFactOfTheDay.useQuery({ date: date as string });
+  const funFactOfTheDay = trpc.fun.getFunFactOfTheDay.useQuery({
+    date: date as string,
+  });
   const featureFlags = trpc.featureFlag.getFeatureFlags.useQuery();
 
   const loadingBlock = (
     <span className="flex h-5 w-5">
-      <span className="animate-ping absolute inline-flex h-5 w-5 rounded-full bg-sky-400 opacity-75"></span>
-      <span className="relative inline-flex rounded-full h-5 w-5 bg-sky-500"></span>
+      <span className="absolute inline-flex h-5 w-5 animate-ping rounded-full bg-sky-400 opacity-75"></span>
+      <span className="relative inline-flex h-5 w-5 rounded-full bg-sky-500"></span>
     </span>
   );
 
@@ -34,12 +36,41 @@ export const FunBlock = (date: string) => {
         <title>F.F.</title>
         <meta name="description" content="Mais um dia, mais um fun fact" />
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="F.F." />
+        <meta
+          property="og:description"
+          content={
+            funFactOfTheDay.data
+              ? funFactOfTheDay.data.funFact.content
+              : "boom :)"
+          }
+        />
+        <meta
+          property="og:image"
+          content={
+            funFactOfTheDay.data
+              ? funFactOfTheDay.data.pokemonOfTheDay.pokemon.artworkUrl
+              : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png"
+          }
+        />
+        <meta
+          name="twitter:image"
+          content={
+            funFactOfTheDay.data
+              ? funFactOfTheDay.data.pokemonOfTheDay.pokemon.artworkUrl
+              : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png"
+          }
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="theme-color" content="#000000" />
       </Head>
-      <main className="text-black bg-white dark:text-white dark:bg-[#15162c]">
+      <main className="bg-white text-black dark:bg-[#15162c] dark:text-white">
         {Header(featureFlags.data)}
-        <div className="min-h-screen flex flex-col items-center gap-10 p-4">
+        <div className="flex min-h-screen flex-col items-center gap-10 p-4">
           <h1 className="text-6xl font-extrabold tracking-tight">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#bd0a0a] to-[#362ad6] dark:from-[#362ad6] dark:to-[#bd0a0a]">{date}</span>
+            <span className="bg-gradient-to-r from-[#bd0a0a] to-[#362ad6] bg-clip-text text-transparent dark:from-[#362ad6] dark:to-[#bd0a0a]">
+              {date}
+            </span>
           </h1>
           {funFactBlock}
           {pokemonBlock}

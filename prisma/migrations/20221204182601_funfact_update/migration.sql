6adffcfc5bebbd1c0ec6fef0a86ddb2772d95f78
pedrofbo <pedrofelipebo@gmail.com>
2023-02-17 22:00:19 -0300

    chore: remove migration files

--- prisma/migrations/20221204182601_funfact_update/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[funFactId]` on the table `FunFactOfTheDay` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "FunFactOfTheDay_funFactId_key" ON "FunFactOfTheDay"("funFactId");
--- prisma/migrations/20230125235037_/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "PokemonOfTheDay_pokemonId_key";
